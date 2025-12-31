b3bed373fb7808af89abf2d4fde8de2880ef0e5a
oliveiradasilvajoaovictor03-afk <oliveiradasilvajoaovictor03@gmail.com>
2025-12-30 21:22:37 -0400

    fix next.config.js (images remotePatterns)

--- next.config.js
+++ next.config.js
@@ -1,29 +1,14 @@
-next.config.js/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'imgur.com',
-      },
+      { protocol: 'https', hostname: 'github.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'raw.githubusercontent.com' },
+      { protocol: 'https', hostname: 'i.imgur.com' },
+      { protocol: 'https', hostname: 'imgur.com' },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
