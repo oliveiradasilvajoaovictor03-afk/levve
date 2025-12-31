7dd32b8c500594b141aec67e3908aee48ae828e0
oliveiradasilvajoaovictor03-afk <oliveiradasilvajoaovictor03@gmail.com>
2025-12-30 20:55:11 -0400

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,29 @@
+next.config.js/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
